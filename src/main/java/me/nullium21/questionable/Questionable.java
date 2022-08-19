44bf6ec5f322bcb447f437c06ea2c29a566f7678
nullium21 <rommyskovets@gmail.com>
2022-08-19 17:24:32 +0300

    Fix modid in ModInitializer

--- src/main/java/me/nullium21/questionable/Questionable.java
+++ src/main/java/me/nullium21/questionable/Questionable.java
@@ -9,7 +9,7 @@ public class Questionable implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("questionable");
 
 	@Override
 	public void onInitialize() {
